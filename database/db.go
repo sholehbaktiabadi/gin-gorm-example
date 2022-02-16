bfd8307daf30bcbc711ba8d1356781d5c76f24e3
sholehbaktiabadi <sholehbaktiabadi@gmail.com>
2022-02-16 14:30:32 +0700

    init: add env loader

--- database/db.go
+++ database/db.go
@@ -1,14 +1,27 @@
 package database
 
 import (
+	"fmt"
+	"os"
 	"v1/user"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func DatabaseConnection() (*gorm.DB, error) {
-	dsn := "host=localhost user=postgres password=root dbname=go_prisma port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	godotenv.Load(".env")
+	var (
+		host     = os.Getenv("DB_HOST")
+		port     = os.Getenv("DB_PORT")
+		username = os.Getenv("DB_USER")
+		password = os.Getenv("DB_PASSWORD")
+		dbname   = os.Getenv("DB_NAME")
+	)
+	print := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v", host, username, password, dbname, port)
+	fmt.Print(print)
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=disable TimeZone=Asia/Jakarta", host, username, password, dbname, port)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
