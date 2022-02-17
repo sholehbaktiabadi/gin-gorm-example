e4507d0d8741e5e36872f6c6c39fdfafa78937d2
sholehbaktiabadi <sholehbaktiabadi@gmail.com>
2022-02-17 10:10:02 +0700

    feat(): add env loader file

---
+++ config/env.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func Env(name string) string {
+	godotenv.Load(".env")
+	switch name {
+	case "DB_HOST":
+		return os.Getenv(name)
+	case "DB_PORT":
+		return os.Getenv(name)
+	case "DB_USER":
+		return os.Getenv(name)
+	case "DB_PASSWORD":
+		return os.Getenv(name)
+	case "DB_NAME":
+		return os.Getenv(name)
+	case "SERVER_PORT":
+		return os.Getenv(name)
+	default:
+		panic(name + " doesnt exist in file .env")
+	}
+}
